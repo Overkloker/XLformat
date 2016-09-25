040869a6f52bfd7f88134e73473e229f1ed3c2da
Lord <overkloker33@gmail.com>
2016-09-25 22:05:19 +0300

    7 block

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by Frank on 24.09.2016.
  */
+$(document).ready(function () {
+  $('.b-slick-wrapper').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow:'<a href="" class="b-slick-prev"></a>',
+    nextArrow:'<a href="" class="b-slick-next"></a>',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
